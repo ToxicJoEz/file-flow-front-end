f5507632db0b305c1db6f05747b83c529dd51b43
ToxicJoEz <latenightgamer20166@gmail.com>
2025-04-06 10:57:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold text-white bg-black p-8">Tsting to see if Tailwind works</h1>
+    </>
+  );
+}
+
+export default App;
